4ef5196aaf71c0a38a397825b94141591d1c991a
L Childs <laurenchilds@users.noreply.github.com>
2023-08-03 12:54:40 -0400

    Update Baseline_params_malaria.m
    Updates to malaria parameters based on revised assumptions (uses malaria therapy data).

--- Baseline_params_malaria.m
+++ Baseline_params_malaria.m
@@ -1,4 +1,4 @@
-%% Malaria parameters taken from Qu and Patterson et al 2023
+%% Malaria parameters taken from Qu and Patterson et al 2023 (except as noted)
 P.bm = 0.6;
 P.bh = 5;
 
@@ -6,10 +6,10 @@ P.betaM = 0.25;
 P.betaD = 0.35;
 P.betaA = 0.03;
 
-P.rD = 1/180;
-P.rA = 1/360;
+P.rD = 1/33.5; % Updated based on treatment assumption using malaria therapy data % 1/180; original Qu and Patterson et al
+P.rA = 1/85; % Updated  using malaria therapy data  % 1/360; original Qu and Patterson et al
 
-P.h = 1/15;
+P.h = 1/26; % Updated based on malaria therapy data % 1/15; original Qu and Patterson et al
 P.muD = 0;
 
 P.cS = 0.75;
@@ -21,8 +21,8 @@ P.de = 5*365;
 P.gamma = 10;
 
 % Fixed immunity parameters
-P.psi = 0.5; % descreasing function in immunity
-P.rho = 0.5; % descreasing 
+P.psi = 0.5; % increasing function in immunity
+P.rho = 0.5; % increasing 
 P.phi = 0.5; % increasing 
 
 % Simple birth/death parameters (lifespan = 80)
