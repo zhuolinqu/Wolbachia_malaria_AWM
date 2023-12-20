af8f67601ad2ecf45bb67052fc5bc3fe9020e449
L Childs <laurenchilds@users.noreply.github.com>
2023-12-20 13:38:57 -0500

    Updates to Plot Parameter Histograms

--- PlotParameterHistograms.m
+++ PlotParameterHistograms.m
@@ -1,8 +1,5 @@
 clear all; clc
 close all
-figure_setups; clf
-figure_setups; clf
-figure_setups; clf
 
 file_dir = 'Results/SA/SA_PRCC/';
 load([file_dir,'PRCC_result_regions_100000_26'],'P','lP_list','sample_label')
@@ -13,25 +10,6 @@ for iP = 1:length(lP_list)
     upper(iP,1) = P.([param,'_upper']);
 end
 [labs,~,~] = SA_output_formatting(lP_list,'',1);
-
-% lower = [P.h_lower P.rA_lower P.rD_lower P.phiU_lower P.phiW_lower P.mufu_lower P.mufw_lower ...
-%     P.sigma_lower P.betaD_lower P.betaA_lower P.betaM_lower ...
-%     P.de_lower P.vw_lower P.ci_lower P.alpha_lower ...
-%     P.phis2_lower P.phir2_lower P.rhos2_lower P.rhor2_lower P.psis2_lower P.psir2_lower...
-%     P.cS_lower P.cE_lower P.cA_lower P.cD_lower P.dummy_lower];
-% upper = [P.h_upper P.rA_upper P.rD_upper P.phiU_upper P.phiW_upper P.mufu_upper P.mufw_upper ...
-%     P.sigma_upper P.betaD_upper P.betaA_upper P.betaM_upper ...
-%     P.de_upper P.vw_upper P.ci_upper P.alpha_upper ...
-%     P.phis2_upper P.phir2_upper P.rhos2_upper P.rhor2_upper P.psis2_upper P.psir2_upper...
-%     P.cS_upper P.cE_upper P.cA_upper P.cD_upper P.dummy_upper];
-% labs={'h','rA','rD','phiU','phiW','mufu','mufw',...
-%     'sigma','betaD','betaA','betaM', ...
-%     'de','vw','ci','alpha',...
-%     'phis2','phir2','rhos2','rhor2','psis2','psir2',...
-%     'cS','cE','cA','cD','dummy'};
-
-% sample_label (1:26) sampling
-% sample_label (27) labels
 
 % Region 1 - 2
 % Region 2 - 6
@@ -54,29 +32,73 @@ for k = [1 2 6 5 4 3]
         subplot(3,3,j1)
         bin_num = 50;
         if j < 27
-        if k==1
-            histogram(sample_label(:,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
-            xlabel(labs{j})
-            set(gca,'fontsize',14)
-            xlim([lower(j) upper(j)])
-            set(gca,'xtick',[lower(j) upper(j)])
+            if k==1
+                histogram(sample_label(:,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
+                xlabel(labs{j})
+                set(gca,'fontsize',14)
+                xlim([lower(j) upper(j)])
+                set(gca,'xtick',[lower(j) upper(j)])
+            else
+                histogram(sample_label(sample_label(:,27)==k,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
+            end
         else
-            histogram(sample_label(sample_label(:,27)==k,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
+            if k==1
+                j2 = 26;
+                histogram(sample_label(:,j2),'binwidth',(upper(j2)-lower(j2))/bin_num,'binlimits',[lower(j2) upper(j2)],'Normalization','count')
+                %xlabel(labs{j2})
+                set(gca,'fontsize',14)
+                xlim([0 .1])
+                %xlim([lower(j2) upper(j2)])
+                %set(gca,'xtick',[lower(j2) upper(j2)])
+                set(gca,'xtick',[],'ytick',[])
+            else
+                histogram(sample_label(sample_label(:,27)==k,j2),'binwidth',(upper(j2)-lower(j2))/bin_num,'binlimits',[lower(j2) upper(j2)],'Normalization','count')
+            end
         end
-        else
-        if k==1
-            j2 = 26;
-            histogram(sample_label(:,j2),'binwidth',(upper(j2)-lower(j2))/bin_num,'binlimits',[lower(j2) upper(j2)],'Normalization','count')
-            xlabel(labs{j2})
-            set(gca,'fontsize',14)
-            xlim([lower(j2) upper(j2)])
-            set(gca,'xtick',[lower(j2) upper(j2)])
-        else
-            histogram(sample_label(sample_label(:,27)==k,j2),'binwidth',(upper(j2)-lower(j2))/bin_num,'binlimits',[lower(j2) upper(j2)],'Normalization','count')
-        end
-        end
-        
+
         hold on
     end
 end
 legend('All','1','2','3a','3b','4')
+
+%% Figures for manuscript
+f3 = figure_setups;
+
+j1 = 1;
+for k = [ 2 6 5 4 3]
+    for j = [4:7 13:14]
+        subplot(2,3,j1)
+        bin_num = 50;
+        histogram(sample_label(sample_label(:,27)==k,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
+        if k==2
+            xlabel(labs{j})
+            xlim([lower(j) upper(j)])
+            set(gca,'xtick',[lower(j) upper(j)])
+            set(gca,'ytick',[0 1000 2000],'yticklabel',[0 .5 1])
+        end
+        hold on
+
+        j1 = j1 + 1;
+    end
+    j1  = 1;
+end
+
+% legend
+f4 = figure_setups;
+set(f4,'Position', [100, 55, 120, 250])
+for k = [ 2 6 5 4 3]
+    j = 4;
+    bin_num = 50;
+    histogram(sample_label(sample_label(:,27)==k,j),'binwidth',(upper(j)-lower(j))/bin_num,'binlimits',[lower(j) upper(j)],'Normalization','count')
+    xlim([0 .1])
+    set(gca,'xtick',[],'ytick',[])
+    hold on
+end
+
+lgd = legend('1','2','3a','3b','4');
+title(lgd,'Region')
+set(lgd,'location','east','box','off')
+
+%%
+print(f3,'-vector', '-depsc', 'SA_par_histogram.eps')
+print(f4,'-vector', '-depsc', 'SA_par_histogram_legend.eps')
