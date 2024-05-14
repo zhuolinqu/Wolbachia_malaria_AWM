3bebc5af044dd997d6c8f7f40204dba89e55b708
Zhuolin Qu <zhuolin.qu@utsa.edu>
2024-05-13 22:13:12 -0400

    updated figure fontsize

--- figure_setups.m
+++ figure_setups.m
@@ -1,14 +1,14 @@
 function fig = figure_setups()
 
-% fig = figure('Position', [100, 55,900, 700]);
-fig = figure('Position',[444   310   400   300]); 
+fig = figure('Position', [100, 55,900, 700]);
+% fig = figure('Position',[444   310   400   300]); 
 global skip_pt
 global colour_r1 colour_r2 colour_r3 colour_r4
 global colour_blue colour_red colour_yellow
 global colour_mat1 colour_mat2 colour_mat3 colour_mat4 colour_mat5 colour_mat6 colour_mat7
 
-% set(0,'defaultLineLineWidth',8,'defaultTextFontSize',35,'defaultTextFontname','CMU Serif Italic','defaultAxesFontSize',35,'defaultLineMarkerSize',8)
-set(0,'defaultLineLineWidth',4,'defaultTextFontSize',25,'defaultTextFontname','CMU Serif Italic','defaultAxesFontSize',25,'defaultLineMarkerSize',8)
+set(0,'defaultLineLineWidth',6,'defaultTextFontSize',30,'defaultTextFontname','CMU Serif Italic','defaultAxesFontSize',30,'defaultLineMarkerSize',8)
+% set(0,'defaultLineLineWidth',4,'defaultTextFontSize',25,'defaultTextFontname','CMU Serif Italic','defaultAxesFontSize',25,'defaultLineMarkerSize',8)
 
 set(0,'defaultAxesColorOrder',lines(5))
 set(0,'defaultLegendInterpreter','latex');
